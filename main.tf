92d80eb51c163b61a578fca0a1a755e1eb747cc4
cbarricm <33128707+cbarricm@users.noreply.github.com>
2024-11-27 22:37:15 -0800

    ec2 lesson 4

--- main.tf
+++ main.tf
@@ -1,107 +1,59 @@
-data "aws_ami" "app_ami" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["979382823631"] # Bitnami
-}
-
-data "aws_vpc" "default" {
-  default = true
-}
-
-module "blog_vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-
-  name = "dev"
-  cidr = "10.0.0.0/16"
-
-  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
- 
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  tags = {
-    Terraform = "true"
-    Environment = "dev"
-  }
-}
-
-module "autoscaling" {
-  source  = "terraform-aws-modules/autoscaling/aws"
-  version = "8.0.0"
-  # insert the 1 required variable here
-  name = "blog"
-  min_size = 1
-  max_size = 2
-
-  vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_group_arns = module.blog_alb.target_group_arns
-  security_groups = [module.blog_sg.security_group_id]
-
-  image_id           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type  
-
-}
-
-module "blog_alb" {
-  source = "terraform-aws-modules/alb/aws"
-
-  name    = "blog-alb"
-  load_balancer_type = "application"
-
-  vpc_id  = module.blog_vpc.vpc_id
-  subnets = module.blog_vpc.public_subnets
-  security_groups = [module.blog_sg.security_group_id]
-
-
-  security_group_egress_rules = {
-    all = {
-      ip_protocol = "-1"
-      cidr_ipv4   = "10.0.0.0/16"
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 4.50"
     }
   }
 
-  listeners = [
-    {
-      port     = 80
-      protocol = "HTTP"
-      target_group_index = 0
-      }
-    ]
+  required_version = ">= 1.4.0"
+}
 
-  target_groups = [
-     {
-      name_prefix      = "blog-"
-      protocol         = "HTTP"
-      port             = 80
-      target_type      = "instance"
-     }
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_instance" "lesson_04" {
+  ami           = "ami-0c7c4e3c6b4941f0f"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [
+    aws_security_group.sg_ssh.id,
+    aws_security_group.sg_https.id
   ]
 
   tags = {
-    Environment = "Development"
-    Project     = "Example"
+    Name      = "Lesson-04-VM-SG"    
   }
 }
 
-module "blog_sg" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "5.2.0"
-  name = "blog_new"
+resource "aws_security_group" "sg_ssh" {
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "tcp"
+    from_port   = 22
+    to_port     = 22
+  }
 
-  vpc_id = module.blog_vpc.vpc_id
+  egress {
+    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+  }
+}
 
-  ingress_rules = ["http-80-tcp","https-443-tcp"]
-  ingress_cidr_blocks = ["0.0.0.0/0"]
+resource "aws_security_group" "sg_https" {
+  ingress {
+    cidr_blocks = ["192.168.0.0/16"]
+    protocol    = "tcp"
+    from_port   = 443
+    to_port     = 443
+  }
 
-  egress_rules = ["all-all"]
-  egress_cidr_blocks = ["0.0.0.0/0"]
+  egress {
+    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+  }
 }
